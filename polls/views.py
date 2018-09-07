ea608757810a880e99c48885cff1295aa3baf730
Dmitriy Veshniakov <dv@paypro.ru>
2018-09-07 16:00:47 +0700

    * пространства имен в названиях URL-ов

--- polls/views.py
+++ polls/views.py
@@ -11,6 +11,7 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+    #return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
